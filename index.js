5e7eb36994e20755dc7b08fa423634261c51af5c
David Bulka <davidbulka@gmail.com>
2020-05-08 01:15:30 +0300

    filtering holidays

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var Hebcal = require('hebcal');
 Hebcal.LANGUAGE
-
+const ex = ["ערב שבת", "ראש חודש"]
 var year = new Hebcal(5780);
 year.setCity('Jerusalem');
 var holidaysConcat = [].concat(Object.values(year.holidays))
-console.log(holidaysConcat.map(ec=>ec[0].getDesc('h')));
+console.log(holidaysConcat.map(ec=>ec[0].getDesc('h')).filter(d=>d.indexOf(ex[0]) === -1 &&
+                                                                d.indexOf(ex[1]) === -1));
 //console.log(holidaysConcat);
 
