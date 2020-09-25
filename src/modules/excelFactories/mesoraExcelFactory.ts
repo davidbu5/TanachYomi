858ab822e44c3b28d774e1ecaf2b5c337d2fe89a
David Bulka <davidbulka@gmail.com>
2020-09-25 16:37:38 +0300

    mesora excel factory complete

--- src/modules/excelFactories/mesoraExcelFactory.ts
+++ src/modules/excelFactories/mesoraExcelFactory.ts
@@ -1,5 +1,6 @@
 import { Day } from "../../models/day";
 var xl = require('excel4node');
+var gematriya = require('gematriya');
 
 const basicBorderStyle = {
     style: 'thin',
@@ -14,7 +15,7 @@ function getBorderStyle(wb, right?, left?, top?, bottom?) {
     return wb.createStyle({ border: settings })
 }
 export function createMesoraExcelFromWeeks(weeks: Day[][], yearNum?: number, objectToWriteOn?: any) {
-    
+
     var wb = new xl.Workbook({
         defaultFont: {
             size: 8,
@@ -23,26 +24,35 @@ export function createMesoraExcelFromWeeks(weeks: Day[][], yearNum?: number, obj
     });
 
     var sheet = wb.addWorksheet('Calendar', {
+        'margins': {
+            'top': 0.68
+        },
+
         sheetView: {
             rightToLeft: true
         },
         pageSetup: {
-            paperSize: 'A4_PAPER'
+            paperSize: 'LETTER_PAPER'
         },
         sheetFormat: {
-            defaultColWidth: 11,
+            defaultColWidth: 9.6,
         }
     });
-
     // TODO: get gematria year
-    addSiteHeaderToSheet(wb, sheet)
+    addSiteHeaderToSheet(wb, sheet, yearNum)
     addWeekdaysHeaderToSheet(wb, sheet)
     weeks.forEach((week, index) => addWeekToSheet(wb, sheet, index, week))
+
+    for (const i of [0, 1, 2, 3, 4, 5, 6]) {
+        sheet.column(i * 2 + 1).setWidth(11.2);
+        sheet.column(i * 2 + 2).setWidth(9.6);
+    }
+    sheet.column(14).setWidth(5);
 
     wb.write(`${yearNum ? yearNum + " " : ""}Regular.xlsx`, objectToWriteOn)
 }
 
-function addSiteHeaderToSheet(wb, sheet) {
+function addSiteHeaderToSheet(wb, sheet, yearNum: number) {
 
     const centerWrappedTextAlign = wb.createStyle({
         alignment: {
@@ -58,7 +68,7 @@ function addSiteHeaderToSheet(wb, sheet) {
             underline: true,
             size: 14
         },
-        '‏לוח תנ"ך יומי תש"פ',
+        getCalendarName(yearNum),
         {
             name: "Guttman Adii",
             underline: false,
@@ -71,32 +81,64 @@ function addSiteHeaderToSheet(wb, sheet) {
             bold: true,
             size: 11
         },
-        'לימוד כל התנ"ך בשנה אחת - ע"פ חלוקת ה"סדרים" של המסורה',
-        "\n",
+        'לימוד כל התנ"ך בשנה אחת - ע"פ חלוקת ה"סדרים" של המסורה'
+    ]
+
+    const secondHeader = [
         {
             name: "Arial",
             size: 8
         },
-        "לפרטים נוספים: www.tanachyomi.co.il"
+        'לפרטים נוספים: www.tanachyomi.co.il'
     ]
 
     sheet.cell(1, 1, 1, 7 * 2, true)
         .string(header)
         .style(centerWrappedTextAlign);
 
-    sheet.row(1).setHeight(29.40)
+    sheet.cell(2, 1, 2, 7 * 2, true)
+        .string(secondHeader)
+        .style(centerWrappedTextAlign);
+
+    sheet.row(1).setHeight(15.75);
+    sheet.row(2).setHeight(9.75);
+}
+
+function getCalendarName(yearNum: number) {
+    const withoutThousands = yearNum - (yearNum - (yearNum % 1000));
+    const yearName = gematriya(withoutThousands);
+    const title = `‏לוח תנ"ך יומי ${yearName}`;
+    return title;
 }
 
 function addWeekdaysHeaderToSheet(wb, sheet) {
-    const largerFont = wb.createStyle({ font: { size: 12 } })
+    const weekdayHeaderStyle = wb.createStyle({
+        font: {
+            size: 11
+        },
+        alignment: {
+            veritcal: 'center'
+        }
+    });
+
     const days = ['ראשון', 'שני', 'שלישי', 'רביעי', 'חמישי', 'שישי', 'שבת']
     days.forEach((day, index) => {
-        sheet.cell(2, index * 2 + 1, 2, index * 2 + 2, true).string(day).style(getBorderStyle(wb, true, true, true, true)).style(largerFont)
+        sheet.cell(3, index * 2 + 1, 3, index * 2 + 2, true).string(day).style(getBorderStyle(wb, true, true, true, true)).style(weekdayHeaderStyle)
+        sheet.cell(88, index * 2 + 1, 88, index * 2 + 2, true).string(day).style(getBorderStyle(wb, true, true, true, true)).style(weekdayHeaderStyle)
     })
-    sheet.row(2).setHeight(12)
+
+    sheet.row(3).setHeight(12)
+    sheet.row(88).setHeight(12)
 }
 
 function addWeekToSheet(wb, sheet, weekIndex: number, week: Day[]) {
+    let firstRowIndex = weekIndex * 3 + 4;
+    if (firstRowIndex >= 88) {
+        firstRowIndex++;
+    }
+    sheet.row(firstRowIndex).setHeight(9)
+    sheet.row(firstRowIndex + 1).setHeight(8)
+    sheet.row(firstRowIndex + 2).setHeight(11.5)
     week.forEach((day, dayIndex) => addDayToSheet(wb, sheet, weekIndex, dayIndex, day))
 }
 
@@ -104,44 +146,48 @@ function addDayToSheet(wb, sheet, weekIndex: number, dayIndex: number, day: Day)
 
     const smallBoldFont = wb.createStyle({ font: { bold: true } })
     const largeFont = wb.createStyle({ font: { size: 11 } })
-    const centerTextAlign = wb.createStyle({ alignment: { horizontal: 'center' } })
-    const rightTextAlign = wb.createStyle({ alignment: { horizontal: 'right' } })
-    
-    const startIndexFromLeft = dayIndex * 2 + 1;
-    const startIndexFromTop = weekIndex * 3 + 3;
+    const centerTextAlign = wb.createStyle({ alignment: { horizontal: 'center', vertical: 'center' } })
+    const rightTextAlign = wb.createStyle({ alignment: { horizontal: 'right', vertical: 'center' } })
+    const leftTextAlign = wb.createStyle({ alignment: { horizontal: 'left', vertical: 'center' } })
+    const centerVerticalAlign = wb.createStyle({ alignment: { vertAlign: 'center', vertical: 'center' } })
 
-    sheet.cell(startIndexFromTop, startIndexFromLeft).string("").style(getBorderStyle(wb, false, true))
-    sheet.cell(startIndexFromTop, startIndexFromLeft + 1).string("").style(getBorderStyle(wb, true))
+    const startIndexFromLeft = dayIndex * 2 + 1;
+    let startIndexFromTop = weekIndex * 3 + 4;
+    if (startIndexFromTop >= 88) {
+        startIndexFromTop++;
+    }
+
+    sheet.cell(startIndexFromTop, startIndexFromLeft).string("").style(getBorderStyle(wb, false, true)).style(centerVerticalAlign)
+    sheet.cell(startIndexFromTop, startIndexFromLeft + 1).string("").style(getBorderStyle(wb, true)).style(centerVerticalAlign)
 
     sheet.cell(startIndexFromTop + 2, startIndexFromLeft,
-        startIndexFromTop + 2, startIndexFromLeft + 1, true).string("").style(getBorderStyle(wb, true, true, false, true))
+        startIndexFromTop + 2, startIndexFromLeft + 1, true).string("").style(getBorderStyle(wb, true, true, false, true)).style(centerVerticalAlign)
 
     sheet.cell(startIndexFromTop + 1, startIndexFromLeft,
-        startIndexFromTop + 1, startIndexFromLeft + 1, true).string("").style(getBorderStyle(wb, true, true))
-
+        startIndexFromTop + 1, startIndexFromLeft + 1, true).string("").style(getBorderStyle(wb, true, true)).style(centerVerticalAlign)
 
     if (day) {
         sheet.cell(startIndexFromTop, startIndexFromLeft).string(day.hebrewRepresentation)
-        sheet.cell(startIndexFromTop, startIndexFromLeft + 1).string(day.gregRepresentation).style(rightTextAlign)
+        sheet.cell(startIndexFromTop, startIndexFromLeft + 1).string(day.gregRepresentation).style(leftTextAlign).style(centerVerticalAlign)
 
         if (day.holidayName) {
             sheet.cell(startIndexFromTop + 1, startIndexFromLeft,
-                startIndexFromTop + 1, startIndexFromLeft + 1).string(day.holidayName).style(smallBoldFont).style(centerTextAlign)
+                startIndexFromTop + 1, startIndexFromLeft + 1).string(day.holidayName).style(smallBoldFont).style(centerTextAlign).style(centerVerticalAlign)
         }
 
         if (day.seder) {
             sheet.cell(startIndexFromTop + 2, startIndexFromLeft,
                 startIndexFromTop + 2, startIndexFromLeft + 1).string([
                     {
-                        size: 12
+                        size: 11
                     },
                     day.seder.bookName,
                     {
-                        size: 10
+                        size: 9
                     },
                     " ס' ",
                     {
-                        size: 12
+                        size: 11
                     },
                     day.seder.sederInBook
                 ])
@@ -151,7 +197,7 @@ function addDayToSheet(wb, sheet, weekIndex: number, dayIndex: number, day: Day)
             day.parashatShavua.indexOf('חול המועד') === -1 &&
             day.parashatShavua.indexOf('סוכות') === -1) {
             sheet.cell(startIndexFromTop + 2, startIndexFromLeft,
-                startIndexFromTop + 2, startIndexFromLeft + 1).string(day.parashatShavua).style(smallBoldFont)
+                startIndexFromTop + 2, startIndexFromLeft + 1).string(day.parashatShavua).style(smallBoldFont).style(centerVerticalAlign)
         }
     }
 }
--- tests/excels.ts
+++ tests/excels.ts
@@ -3,11 +3,14 @@ import { createNotMesoraExcelFromWeeks } from '../src/modules/excelFactories/not
 import { createMesoraExcelFromWeeks } from '../src/modules/excelFactories/mesoraExcelFactory';
 import { createMesoraSmallExcelFromWeeks } from '../src/modules/excelFactories/mesoraSmallExcelFactory';
 
-const years = [5780, 5782];
+const years = [5782];
 
 years.forEach(yearNum => {
     const year = getTanachLearningYear(yearNum);
-    createNotMesoraExcelFromWeeks(year, yearNum)
+    //createNotMesoraExcelFromWeeks(year, yearNum)
     createMesoraExcelFromWeeks(year, yearNum)
-    createMesoraSmallExcelFromWeeks(year, yearNum)
+    //createMesoraSmallExcelFromWeeks(year, yearNum);
+
+    var cp = require("child_process");
+    cp.exec("start excel \"C:/Dev/Node/TanachYomi/tests/5782 Regular.xlsx\""); // notice this without a callback..
 })
